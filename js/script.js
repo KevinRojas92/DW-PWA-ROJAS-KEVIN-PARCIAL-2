1462589560701e4270d0c66f4ad5db89a435ac4b
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-14 00:28:00 -0300

    Solving fist function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let buscador = document.getElementById("buscador");
+
+buscador.addEventListener('change', buscarPelicula => {
+    buscarPelicula.preventDefault();
+
+    let busqueda = buscador.value;
+
+    localStorage.setItem("busqueda", `${busqueda}`);
+});
