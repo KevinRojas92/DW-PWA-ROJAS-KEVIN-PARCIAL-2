511945225ea6ecdf48756ce43a675f94d0308cb3
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-14 23:34:39 -0300

    Try to add a new api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const url = 'https://moviesdatabase.p.rapidapi.com/titles/random?limit=5&list=most_pop_movies';
+const options = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '76a66db868msh7661b2572bec535p1d67acjsn23d92673c089',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+};
+
+try {
+    const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+    console.error(error);
+}
+
 let buscador = document.getElementById("buscador");
 
 buscador.addEventListener('change', buscarPelicula => {
