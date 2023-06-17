5dc2b134eb003706c627d6cf7a43cffcae65796e
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-17 12:17:44 -0300

    Create and add status cheaker

---
+++ js/status_conexion.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let main = document.getElementById("main");
+let divStatus;
+
+window.addEventListener("offline", checStatus => {
+    divStatus = document.createElement("div");
+    divStatus.setAttribute("id", "statusConexion");
+    divStatus.innerHTML += "<p>Se perdió la conexión a internet</p>";
+    main.appendChild(divStatus);
+});
+
+window.addEventListener("online", checStatus => {
+    divStatus = document.createElement("div");
+    divStatus.setAttribute("id", "statusConexion");
+    divStatus.innerHTML += "<p>Volvió la conexión a internet</p>";
+    main.appendChild(divStatus);
+});
+
+if (!navigator.onLine) {
+    console.log("El usuario está sin conexión");
+}
+
