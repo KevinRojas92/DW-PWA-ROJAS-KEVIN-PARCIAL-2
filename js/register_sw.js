5e2eebc1a1172bf560cf733a7fafd12de18e61e9
Kevin Rojas <kevinrojasdg@gmail.com>
2023-06-17 13:03:18 -0300

    Do again

--- js/register_sw.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('serviceWorker.js')
-    .then ((message) => {
-        console.log('Service Worker esta listo y funcionando');
-    })
-} else {
-    console.log('Este browser no soporta Service Worker');
-}
